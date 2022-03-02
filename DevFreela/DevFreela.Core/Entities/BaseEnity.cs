074b8c11e28bdfdb7f29500ef9d4d238000cbc29
Fabio Macieira <118069@pmenos.com.br>
2022-03-01 21:14:50 -0300

    remover project devfreela

--- DevFreela/DevFreela.Core/Entities/BaseEnity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevFreela.Core.Entities
-{
-    public abstract class BaseEnity
-    {
-        public BaseEnity() { }
-        public int Id { get; private set; }
-    }
-}
--- DevFreela/DevFreela.Core/Entities/UserSkill.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevFreela.Core.Entities
-{
-    public class UserSkill : BaseEnity
-    {
-        public int IdUser { get; private set; }
-        public int idSkill { get; private set; }
-
-    }
-}
